5ab86258685c64a7602dec456044b0b39a8f98b1
Denis <denyashin@yandex.ru>
2022-11-29 00:03:43 +0300

    Change to if-else

--- Инструкция if  else.py
+++ Инструкция if  else.py
@@ -4,11 +4,5 @@ print(f'Вы ввели {speed} км/ч')
 
 if speed > 24 and speed < 56:
     print('Скорость нормальная')
-elif speed < 24:
+else:
     print('Скорость аварийная')
-elif speed > 56:
-    print('Скорость аварийная')
-elif speed == 24:
-    print('Скорость на грани')
-elif speed == 56:
-    print('Скорость на грани')

4e8cc4aa7f938460d8844fcc90dba6bde937c663
Denis <denyashin@yandex.ru>
2022-11-29 16:40:25 +0300

    Add file

---
+++ Рисунок 2 треугольника.py
@@ -0,0 +1,33 @@
+import turtle
+
+turtle.hideturtle()
+turtle.fillcolor('hot pink')
+turtle.begin_fill()
+turtle.left(135)
+turtle.forward(100)
+turtle.left(89.9)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.left(0.1)
+turtle.forward(100)
+turtle.right(89.6)
+turtle.forward(100)
+turtle.right(90.1)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+
+#turtle.left(52)
+#turtle.forward(100)
+#turtle.right(135)
+#turtle.forward(100)
+#turtle.right(135)
+#turtle.forward(141)
+
+turtle.end_fill()
+turtle.done()

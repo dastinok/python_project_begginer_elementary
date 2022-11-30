3548c477a4b6f7264c6a913f56d551ee18ba82c1
Denis <denyashin@yandex.ru>
2022-11-30 21:17:28 +0300

    Добавил функцию в цикл while для выбора продолжить или завершить

--- Прога с while.py
+++ Прога с while.py
@@ -1,13 +1,28 @@
-MAX_TEMP = 103.56
-temperature = float(input('Введите температуру в Цельсиях: '))
-print(f'Вы ввели {temperature}С\N{DEGREE SIGN}')
-
-
-while temperature > MAX_TEMP:
-    print('Убавь температуру! Подожди 5 минут и повтори процесс')
+def convert():
+    MAX_TEMP = 103.56
     temperature = float(input('Введите температуру в Цельсиях: '))
+    print(f'Вы ввели {temperature}С\N{DEGREE SIGN}')
 
-print('Температура приемлемая. Проверьте через 15 минут.')
+
+    while temperature > MAX_TEMP:
+        print('Убавь температуру! Подожди 5 минут и повтори процесс')
+        temperature = float(input('Введите температуру в Цельсиях: '))
+
+    print('Температура приемлемая. Проверьте через 15 минут.')
+
+convert()
+while True:
+    flag = input('Ещё раз? [да/нет]: ')
+
+    if flag == 'да':
+        convert()
+    else:
+        break
+
+
+
+
+
 
 
 

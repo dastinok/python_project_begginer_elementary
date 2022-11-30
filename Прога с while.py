d5d05df1f27354936302fadf5a9ab80644fa5f29
Denis <denyashin@yandex.ru>
2022-11-30 20:40:18 +0300

    Add file

---
+++ Прога с while.py
@@ -0,0 +1,18 @@
+MAX_TEMP = 103.56
+temperature = float(input('Введите температуру в Цельсиях: '))
+print(f'Вы ввели {temperature}С\N{DEGREE SIGN}')
+
+
+while temperature > MAX_TEMP:
+    print('Убавь температуру! Подожди 5 минут и повтори процесс')
+    temperature = float(input('Введите температуру в Цельсиях: '))
+
+print('Температура приемлемая. Проверьте через 15 минут.')
+
+
+
+
+
+
+
+

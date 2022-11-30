8045e8d23e0d9de1f5f3cbbc68f989200363941b
Denis <denyashin@yandex.ru>
2022-11-30 19:35:08 +0300

    Add file

---
+++ Треугольник внутри треугольника.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.hideturtle()
+turtle.fillcolor('firebrick2')
+turtle.begin_fill()
+turtle.right(45)
+turtle.forward(200)
+turtle.right(135)
+turtle.forward(270)
+turtle.right(132)
+turtle.forward(195)
+turtle.end_fill()
+
+turtle.left(180)
+turtle.forward(195)
+turtle.right(160)
+turtle.forward(330)
+turtle.right(132.5)
+turtle.forward(341)
+
+
+turtle.done()

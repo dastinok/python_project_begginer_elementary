be57e0a906fe3aa4f7708b5ee5ae0e53f37b4275
Denis <denyashin@yandex.ru>
2022-12-03 18:21:15 +0300

    Add file

---
+++ Квадрат с точками.py
@@ -0,0 +1,98 @@
+import turtle
+
+turtle.pensize(3)
+turtle.dot()
+turtle.dot(15, "blue")
+
+
+turtle.goto(200, 200)
+turtle.dot(15, "red")
+
+turtle.goto(200, -200)
+turtle.dot(15, "orange")
+
+turtle.goto(0,0)
+
+turtle.goto(-200, -200)
+turtle.dot(15, "black")
+
+turtle.goto(-155, -200)
+turtle.penup()
+turtle.goto(-110, -200)
+
+turtle.pendown()
+turtle.goto(-10, -200)
+
+turtle.penup()
+turtle.goto(35, -200)
+
+turtle.pendown()
+turtle.goto(130, -200)
+
+turtle.penup()
+turtle.goto(175, -200)
+
+turtle.pendown()
+turtle.goto(200, -200)
+turtle.dot(15, "orange")
+
+turtle.penup()
+turtle.goto(180, -180)
+
+turtle.goto(0,0)
+turtle.dot(15, "blue")
+
+turtle.goto(200, 200)
+turtle.dot(15, "red")
+
+turtle.goto(192, 200)
+
+turtle.pendown()
+turtle.goto(155, 200)
+
+turtle.penup()
+turtle.goto(120, 200)
+
+turtle.pendown()
+turtle.goto(35, 200)
+
+turtle.penup()
+turtle.goto(-20, 200)
+
+turtle.pendown()
+turtle.goto(-110, 200)
+
+turtle.penup()
+turtle.goto(-150, 200)
+
+turtle.pendown()
+turtle.goto(-200, 200)
+turtle.dot(15, "purple2")
+
+turtle.penup()
+turtle.goto(-195, 195)
+
+turtle.pendown()
+turtle.goto(0,0)
+turtle.dot(15, "blue")
+
+turtle.penup()
+turtle.goto(-200, -200)
+
+turtle.pendown()
+turtle.goto(-200, 195)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()

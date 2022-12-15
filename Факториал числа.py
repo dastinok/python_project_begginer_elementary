10b1940c0dfd403c27cfa52fae2a0e257f0497e6
Denis <denyashin@yandex.ru>
2022-12-15 20:53:37 +0300

    Add file

---
+++ Факториал числа.py
@@ -0,0 +1,28 @@
+def convert():
+    print('---------Вычисление Факториала числа--------')
+
+    number = int(input('Введите неотрицательное целое число: '))
+
+    print('Число\t\tФакториал')
+
+    factorial = 1
+    for count in range(number):
+        count += 1
+        factorial = count * factorial
+
+        print(f'{count}\t\t\t\t{factorial}')
+
+convert()
+while True:
+    flag = input('Ещё раз? [да/нет]: ')
+
+    if flag == 'да':
+        convert()
+    else:
+        break
+
+
+
+
+
+
